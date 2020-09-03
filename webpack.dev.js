d27a047caa1fa293f5b745b3a4ae9f0e682009c6
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-09-03 22:16:05 +0300

    eslint fixed

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
-const {merge} = require('webpack-merge');
+/* eslint-disable import/no-extraneous-dependencies */
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
 });
